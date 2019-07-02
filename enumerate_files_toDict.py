af7be6fcbdd2f22efb6e95c2278fd3875c0b92f9
Himanshu Bhardwaj <himanbhard@gmail.com>
2019-07-02 09:22:41 -0400

    added enum method
    For files in directory, add them to dict. Use this is if you want to put file names in a dict, and then use the dict for other operations.

---
+++ enumerate_files_toDict.py
@@ -0,0 +1,11 @@
+import os
+
+logFilepath = r"\folder\folder\Logs\PS02\EstesPSTwoPDRouteOptimization"
+searchString = "These are not the droids you are looking for"
+
+def enumerate_files():
+    my_files = {}                 
+    for i, e in enumerate(os.listdir(logFilepath)):
+        my_files[i] = e
+    print(my_files)
+
